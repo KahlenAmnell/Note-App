80c8f5bbfbdb9828bb0c7dd9d8b7e00836db838b
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2024-10-30 10:58:20 +0100

    Remove ignored files

---
+++ Entities/Dashboard.cs
@@ -0,0 +1,10 @@
+﻿namespace Note_App_API.Entities
+{
+    public class Dashboard
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User UserName { get; set; }
+        public List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Note_App_API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public virtual Dashboard Dashboard { get; set; }
+        public virtual List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
