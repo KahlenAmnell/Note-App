046d2a8f351fb16369544e346fa966e2c84e55f6
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2024-10-30 11:07:39 +0100

    Add DbContext class

---
+++ Entities/NoteDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Note_App_API.Entities
+{
+    public class NoteDbContext : DbContext
+    {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options) : base(options) { }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Dashboard> Dashboards { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
